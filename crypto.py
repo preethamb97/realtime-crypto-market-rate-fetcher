4a2c7f90d9eefdff2fb5eea6285411229b9286a3
Preetham <preethamb97@gmail.com>
2020-04-06 22:09:18 +0530

    fix

--- crypto.py
+++ crypto.py
@@ -3,8 +3,33 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 import time
+
+def urlMaker(currencyName):
+    url = 'https://finance.yahoo.com/quote/'+currencyName+'=X'
+    # url = 'https://finance.yahoo.com/quote/USDJPY=X/'
+    return url
+
+def render(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
+
+def regexRenderedData(soup, currency):
+    rate = re.findall(r'data-reactid="14">(.*?)</span>', str(soup))
+    timeValue = re.findall(r'As of  (.*?) GMT. Market open.</span>', str(soup))
+    daysRange = re.findall(r'data-test="DAYS_RANGE-value">(.*?)</td></tr>', str(soup))
+    bid = re.findall(r'<span class="Trsdu\(0.3s\)" data-reactid="26">(.*?)</span></td></tr>', str(soup))
+    ask = re.findall(r'data-test="ASK-value"><span class="Trsdu\(0.3s\)" data-reactid="42">(.*?)</span>', str(soup))
+    datetimeVal = 'UTC:'+str(time.localtime().tm_year)+':'+str(time.localtime().tm_mon)+':'+str(time.localtime().tm_mday)+':'+str(time.localtime().tm_hour)+':'+str(time.localtime().tm_min)+':'+str(time.localtime().tm_sec)
+    curencyString = currency
+    currentRate = rate[0]
+    daysRangeValue = daysRange[0]
+    bidValue = bid[0]
+    askValue = ask[0]
+    allInOne = str(datetimeVal)+'-'+str(curencyString)+'-'+str(currentRate)+'-'+str(daysRangeValue)+'-'+str(bidValue)+'-'+str(askValue)
+    return allInOne
+
 keyvalue = {}
-VS = 'USDJPY'
 allcurrency = '''
     1 GBPNZD
     2 GBPUSD
@@ -28,41 +53,14 @@ allcurrency = '''
 individualcurrency = allcurrency.split()
 for i in range(0,len(individualcurrency),2):
     keyvalue[individualcurrency[i]] = individualcurrency[i+1]
-print(allcurrency)
-inptType = input('ENTER THE CURRENCY TYPE : ')
-VS = keyvalue[inptType]
+
 while True:
-# print('utc = ', time.gmtime())
-    url = 'https://finance.yahoo.com/quote/'+VS+'=X'
-    # url = 'https://finance.yahoo.com/quote/USDJPY=X/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    currency = re.findall('quote/(.*?)=', str(url))
-    rate = re.findall(r'data-reactid="14">(.*?)</span>', str(soup))
-    timeValue = re.findall(r'As of  (.*?) GMT. Market open.</span>', str(soup))
-    daysRange = re.findall(r'data-test="DAYS_RANGE-value">(.*?)</td></tr>', str(soup))
-    bid = re.findall(r'<span class="Trsdu\(0.3s\)" data-reactid="26">(.*?)</span></td></tr>', str(soup))
-    ask = re.findall(r'data-test="ASK-value"><span class="Trsdu\(0.3s\)" data-reactid="42">(.*?)</span>', str(soup))
-    datetimeVal = 'UTC:'+str(time.localtime().tm_year)+':'+str(time.localtime().tm_mon)+':'+str(time.localtime().tm_mday)+':'+str(time.localtime().tm_hour)+':'+str(time.localtime().tm_min)+':'+str(time.localtime().tm_sec)
-    curencyString = currency[0]
-    currentRate = rate[0]
-    utcTime = timeValue[0]
-    daysRangeValue = daysRange[0]
-    bidValue = bid[0]
-    askValue = ask[0]
-    allInOne = str(datetimeVal)+'-'+str(curencyString)+'-'+str(currentRate)+'-'+str(utcTime)+'-'+str(daysRangeValue)+'-'+str(bidValue)+'-'+str(askValue)
-    print(allInOne)
-    fileName = curencyString+'.txt'
-    fw = open(fileName, 'a')
-    fw.write(str(allInOne))
-    fw.write('\n')
-    fw.close()
-    # print('time = ', time.localtime().tm_hour,':',time.localtime().tm_min,':',time.localtime().tm_sec)
-    # print(currency[0])
-    # print('rate = ', rate[0])
-    # print('time = ', timeValue[0])
-    # print('days Range = ', daysRange[0])
-    # print('bid = ', bid[0])
-    # print('ask = ', ask[0])
-    # print('-----------------------------------')
+    for i in keyvalue:
+        # print(keyvalue[i])
+        url = urlMaker(keyvalue[i])
+        soup = render(url)
+        allInOne = regexRenderedData(soup, keyvalue[i])
+        
+        
+        print(allInOne)
     time.sleep(0.01)
